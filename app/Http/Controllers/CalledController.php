bf1a91e2ed2222f9b8899d1ddf4330323f424be1
RuanSdev <ruancom01@gmail.com>
2024-03-13 17:41:14 -0300

    Create the method update

--- app/Http/Controllers/CalledController.php
+++ app/Http/Controllers/CalledController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CalledRequest;
+use App\Http\Requests\CalledUpdateRequest;
 use App\Models\Called;
 use App\Models\Establishment;
 use Illuminate\Support\Facades\Auth;
+use Spatie\FlareClient\Http\Exceptions\NotFound;
 
 class CalledController extends Controller
 {
+
     /**
      * Display a listing of the resource.
      */
@@ -46,9 +49,19 @@ class CalledController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(CalledRequest $request, Called $called)
+    public function update(CalledUpdateRequest $request, Called $called, string $uuid)
     {
-        //
+        $validate = $request->validated();
+        $called = Called::find($uuid);
+        if (empty($called)) {
+            return response()->json(['error' => 'Not Found'], 404);
+        }
+        $called->update($validate);
+
+        return response()->json([
+            "status" => "assigned",
+            'data' => $called,
+        ], 200);
     }
 
     /**
